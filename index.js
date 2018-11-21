7c9464fe2ee1054d6bf0bb4feb98add45935f6ef
Alexander Carlin <ahcarlin@gmail.com>
2018-11-21 18:37:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function increaseRankBy(n) {
 }
 
 function deepestChild() {
-  let deepList = document.getElementById('grand-node');
+  let deepList = document.querySelectorAll('#grand-node div');
   let deepest;
   
   if (deepList.hasChildNodes()) {
